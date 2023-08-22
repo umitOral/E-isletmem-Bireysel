cbe18112ec4040a941a1b2ae5a4e13e9c9486cac
umitOral <umit.oralmat10@gmail.com>
2023-08-22 13:23:59 +0300

    revert version

--- controller/pageController.js
+++ controller/pageController.js
@@ -144,7 +144,7 @@ const getSessionsPage = async (req, res) => {
         const sessions = await Sessions.find({}).sort({ time: 1 }).populate(["user", "doctor"])
         const services = await Service.find({ activeorNot: { $ne: false } })
 
-        res.status(200).render("sessions3", {
+        res.status(200).render("sessions", {
             link: "sessions",
             doctors,
             users,
