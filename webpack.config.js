270e293cddf77af6e1b0e4ddac8f365426fc685c
umit <umit.oralmat10@gmail.com>
2024-01-16 13:09:34 +0300

    webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import webpack from 'webpack'
+
+const module={
+    entry: ["@babel/polyfill","./public/js/appointments.js"],
+    output: {
+        path: path.resolve(__dirname+"/public", 'bundles'), 
+        filename: 'appointments.js'
+    },
+    mode: "development",
+    
+    module: {
+        rules: [
+            {
+              test: /\.js$/,
+              exclude: /(node_modules)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ["@babel/preset-env"]
+                }
+              }
+            }
+          ]
+    }
+};
+
+
+export default module
