631d9c3ed6c7d4bb7949c3e2798e4fa0d6c6f4f0
umitOral <126783898+umitOral@users.noreply.github.com>
2024-09-13 16:06:08 +0300

    popup desgin changed and add popup to login page

--- public/js/login.js
+++ public/js/login.js
@@ -5,9 +5,12 @@ const request = new Request();
 
 const loginForm = document.querySelector("#login-form");
 const messageModal = document.querySelector(".res-message");
+const successPopup = document.querySelector(".success_popup_wrapper")
 
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  messageModal.style="display:none"
+  messageModal.textContent=""
   let data={
     email:loginForm.email.value,
     password:loginForm.password.value,
@@ -16,16 +19,20 @@ loginForm.addEventListener("submit", (e) => {
     .postWithUrl("./login",data)
     .then((response) => {
       console.log(response)
-      messageModal.style="display:block"
-      messageModal.textContent=response.message
+      
         if (response.success==true) {
+          successPopup.classList.add("showed")
             setTimeout(() => {
                 window.location.assign("/admin")
-            }, 1000);
+            }, 1500);
+        }else{
+          messageModal.style="display:block"
+          messageModal.textContent=response.message
         }
     })
     .catch((err) => {
       console.log(err);
-      
+      messageModal.style="display:block"
+      messageModal.textContent=response.message
     });
 });
