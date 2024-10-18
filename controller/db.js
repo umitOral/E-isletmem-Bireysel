0adde18d8137daecfaeba143fd80489006f21264
umitOral <126783898+umitOral@users.noreply.github.com>
2024-10-18 08:42:10 +0300

    Update db.js

--- controller/db.js
+++ controller/db.js
@@ -1,44 +1,49 @@
 import mongoose from "mongoose";
 import { CustomError } from "../helpers/error/CustomError.js";
 
-const connect = async () => {
+
+
+const connectGeneralDb = async (dbName) => {
   try {
-    console.log( "connect fonksiyonu")
+    console.log("connect fonksiyonu");
 
     if (process.env.MODE === "development") {
-     
       await mongoose
         .connect(process.env.DB_URI_DEV, {
-          dbName: process.env.DB_NAME_DEV,
+          dbName: dbName,
           useNewUrlParser: true,
           useUnifiedTopology: true,
         })
         .then((response) => {
-          console.log("db development bağlantısı başarılı:" + process.env.DB_NAME_DEV)
-            
+          console.log("db development bağlantısı başarılı:" + dbName);
         })
         .catch((err) => {
-          console.log("yyyy");
+          console.log(err);
         });
     } else {
-      
       await mongoose
         .connect(process.env.DB_URI, {
-          dbName: process.env.DB_NAME,
+          dbName: dbName,
           useNewUrlParser: true,
           useUnifiedTopology: true,
         })
         .then((response) => {
-          console.log("db bağlantısı başarılı:" + process.env.DB_NAME);
-        })
-        
+          console.log("db bağlantısı başarılı:" + dbName);
+        });
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-  
-
 };
 
-export default connect;
+// Var olan bir bayi bağlantısını al
+const getTenantDb = (dbName, modelName,schema) => {
+console.log(dbName)
+  const db = mongoose.connection.useDb(dbName);
+ 
+  const Model = db.model(modelName, schema);
+ console.log(Model)
+  return Model;
+};
 
+export { connectGeneralDb,getTenantDb };
